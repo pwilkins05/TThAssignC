de3819a45a3b17cd78e0c5a5fea5e0ae8c2b52e2
stphnhffmnn <stphnhffmnn@gmail.com>
2016-02-02 13:39:03 -0800

    Broken something
    Please fix so it compiles

---
+++ Stephen.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+//Program wont run. Helpppp!!!!
+int main()
+{
+
+    cout << 'Hello world!' << endl;
+    int i = 0;
+
+    while(i < 1000)
+    (
+        cout << "Looping";
+        i++;
+    )
+    int arraything[5] = {1,1,2,3,4,5};
+
+    for(int i = 0, i < 5; i++)
+    {
+        cout << arraything[i];
+    }
+
+    return 0;
+}
