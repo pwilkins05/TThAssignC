5358122a8b8c8f35bee534b8620e449a951ed1c7
BenMiddleton <pheendish@gmail.com>
2016-02-04 05:38:28 -0800

    Fizz Buzz
    Added JS Fizz Buzz implementation.

---
+++ FizzBuzz.js
@@ -0,0 +1,16 @@
+(function () {
+	var line;
+	
+	// only prints fizz or buzz, not both
+	for (var i = 1; i <= 100; i++) {
+		line = "";
+		
+		if (i % 3 == 0) line = "Fizz";
+		if (i % 5 == 0) line = "Buzz";
+		
+		if (line != "")
+			console.log(line);
+		else 
+			console.log(i);
+	}
+})();
