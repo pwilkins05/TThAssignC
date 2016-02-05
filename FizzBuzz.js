f2b316de7cef026f986d61592834a295c67e84fb
hunterthornton <hunter.thornton@gmail.com>
2016-02-04 16:57:51 -0800

    Gave an idea on a possible fix to printing fizzbuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,11 +2,13 @@
 	var line;
 	
 	// only prints fizz or buzz, not both
+	//Curious on whats this for? seems like its not appending the line?
 	for (var i = 1; i <= 100; i++) {
 		line = "";
 		
 		if (i % 3 == 0) line = "Fizz";
 		if (i % 5 == 0) line += "Buzz";
+		//if (i % 3 == 0 && i % 5 == 0) line = "Fizz Buzz";
 		
 		if (line != "")
 			console.log(line);
